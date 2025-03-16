46370852056a1cc0b4c25ef420c52e08133bfaa3
estherhamiz590 <estherhamiz590@gmail.com>
2025-03-16 20:14:19 +0000

    Add 12.php

---
+++ src/12.php
@@ -0,0 +1,11 @@
+ <?php
+    $math_problem = array(
+        '1 + 1',
+        '2 + 2',
+        '3 + 3',
+        '4 + 4',
+        '5 + 5'
+    );
+    shuffle($math_problem);
+    echo $math_problem[0];
+ ?>
