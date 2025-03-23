4732cb6a62a65e11110bcafbb155a06a4a037369
estherhamiz590 <estherhamiz590@gmail.com>
2025-03-23 10:46:05 +0000

    Add 19.php

---
+++ src/19.php
@@ -0,0 +1,15 @@
+<?php
+$a = 5;
+$b = 10;
+$c = $a + $b;
+$d = $a - $b;
+$e = $a * $b;
+$f = $a / $b;
+g = $a % $b;
+
+echo "The sum is: $c";
+echo "<br>The difference is: $d";
+echo "<br>The product is: $e";
+echo "<br>The quotient is: $f";
+echo "<br>The remainder is: $g";
+?>
